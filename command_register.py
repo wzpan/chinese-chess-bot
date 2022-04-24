344b4572d37dbe39bbd39f7c3fe88ae859f42115
Fox_white <vct.xie@gmail.com>
2022-04-24 15:33:36 +0800

    General improvement

--- command_register.py
+++ command_register.py
@@ -6,38 +6,89 @@ command_register: 用于指令注册
 author: wzpan
 email: m@hahack.com
 """
-
+import asyncio
+import os
+from dataclasses import dataclass
+from typing import Callable, Coroutine, TypeVar, Any, Dict
 
 import qqbot
+from qqbot.core.util.yaml_util import YamlUtil
+
+T = TypeVar('T')
+Coro = Coroutine[Any, Any, T]
 
 
-def command(command_str: str, validate_func=None, invalid_func=None):
-    """
-    指令的装饰器
-    :param command_str: 指令的字符串。例如 `菜单`
-    :param check_param: 是否需要检查参数
-    :param invalid_func: 当参数不符合要求时的处理函数
-    :return: 装饰器
-    """
+class CheckFailed(Exception):
+    pass
 
-    def decorator(func):
-        async def wrapper(*args, **kwargs):
-            if command_str != "" and command_str in args[2].content:
-                # 指令字符串不为空，并且指令匹配成功
-                qqbot.logger.info("command %s" % command_str)
-                # 解析去除指令之后剩下的参数字符串
-                params = args[2].content.split(command_str)[1].strip()
-                if validate_func and not validate_func(params):
-                    # 如果需要检查参数，并且参数为空
-                    # 调用 invalid_func 函数
-                    return await invalid_func(args[1], args[2])
-                # 调用被装饰的处理函数
-                return await func(params, args[1], args[2])
-            else:
-                qqbot.logger.debug("skip command %s" % command_str)
-                # 指令不匹配，跳过
-                return None
 
-        return wrapper
+@dataclass
+class Command:
+    name: str
+    callback: Callable[[str, str, qqbot.Message], Coro[Any]]
+    checks: Callable[[str], bool]
+    on_error: Callable[[BaseException, str, str, qqbot.Message], Coro[Any]]
 
-    return decorator
+
+class Bot:
+    def __init__(self, prefix: str):
+        self.token = qqbot.Token(self.config["bot"]["appid"], self.config["bot"]["token"])
+        self.prefix = prefix
+        self.commands: Dict[str, Command] = {}
+
+    @property
+    def config(self) -> Dict[str, Any]:
+        return YamlUtil.read(os.path.join(os.path.dirname(__file__), "config.yml"))
+
+    def command(
+            self, name: str,
+            checks: Callable[[str], bool] = None,
+            on_error: Callable[[BaseException, str, str, qqbot.Message], Coro[Any]] = None
+    ):
+        """
+        指令的装饰器
+        :param name: 指令的字符串。例如 `菜单`
+        :param checks: 检查函数，取唯一参数 params ，如果返回 False 则指令不会运行
+        :param on_error: 指令发生错误时调用的回调
+        :return: 装饰器
+        """
+
+        def decorator(func: Callable[[str, str, qqbot.Message], Coro[Any]]):
+            if not asyncio.iscoroutinefunction(func):
+                raise TypeError('回调必须是协程。')
+            result = Command(name=name, callback=func, checks=checks, on_error=on_error)
+            self.commands[name] = result
+            return result
+
+        return decorator
+
+    async def process_commands(self, event: str, message: qqbot.Message):
+        qqbot.logger.info("event %s" % event + ",receive message %s" % message.content)
+        if not message.content.startswith(self.prefix):
+            return False
+        params = message.content.removeprefix(self.prefix)
+        split = params.split()
+        if split[0] not in self.commands:
+            return False
+        invoked_command = self.commands[split[0]]
+        if invoked_command.checks:
+            if not invoked_command.checks(params):
+                if invoked_command.on_error:
+                    await invoked_command.on_error(CheckFailed(), params, event, message)
+                return False
+        qqbot.logger.info("command %s" % invoked_command.name)
+        try:
+            await invoked_command.callback(params, event, message)
+        except Exception as e:
+            if invoked_command.on_error:
+                await invoked_command.on_error(e, params, event, message)
+        return True
+
+    async def handel_message(self, event: str, message: qqbot.Message):
+        """
+        定义事件回调的处理
+        :param event: 事件类型
+        :param message: 事件对象（如监听消息是Message对象）
+        """
+
+        await self.process_commands(event, message)
